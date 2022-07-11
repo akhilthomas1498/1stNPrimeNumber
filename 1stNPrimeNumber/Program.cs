df4c20164cedd1f0ce00514208254d4fb98784e7
Akhil P Thomas <akhilthomas1498@gmail.com>
2022-07-11 11:56:34 +0530

    Add project files.

---
+++ 1stNPrimeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1stNPrimeNumber
+{
+    internal class Program
+    {
+        public int div(int i)
+        {
+            int x = 0;
+            for (int j = 2; j <= (i / 2); j++)
+                if (i % j == 0)
+                    x++;
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter the limit: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Program p = new Program();
+            Console.WriteLine("Prime numbers are");
+            for(int i = 2 ; i <= n; i++)
+            {
+                if(0==p.div(i))
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
